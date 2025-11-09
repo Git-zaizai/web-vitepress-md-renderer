08e6cb67062fdc7772796539deec571cd6c3a65c
Git-zaizai <2394276262@qq.com>
2025-11-10 00:40:22 +0800

    调整css防止冲突

--- __test__/main.ts
+++ __test__/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
+import './t.css'
 
-// import App from './App.vue'
-// createApp(App).mount('#app')
+import App from './App.vue'
+createApp(App).mount('#app')
 
-import appDist from './app-dist.vue'
-createApp(appDist).mount('#app')
+// import appDist from './app-dist.vue'
+// createApp(appDist).mount('#app')
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,4 @@ export {
   createMarkdownRenderer,
   disposeMdItInstance
 } from './markdown/markdown'
-export type {
-  BundledLanguage,
-  BundledLanguageInfo,
-  BundledTheme,
-  BundledThemeInfo
-} from 'shiki'
-export {
-  bundledLanguages,
-  bundledLanguagesInfo,
-  bundledThemes,
-  bundledThemesInfo
-} from 'shiki'
-
-export { createHighlighterCore } from 'shiki'
+export * from 'shiki'
